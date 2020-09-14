997eb6bcda66ec05e6066d392ec258d3ef65ac4e
Stephen Clark <stephen.clark2@city-holdings.co.uk>
2020-09-14 10:32:26 +0100

    80

--- TravelRecordApp/ProfilePage.xaml.cs
+++ TravelRecordApp/ProfilePage.xaml.cs
@@ -3,6 +3,7 @@ using SQLite;
 using TravelRecordApp.Model;
 using Xamarin.Forms;
 using System.Linq;
+using System.Collections.Generic;
 
 namespace TravelRecordApp
 {
@@ -24,6 +25,23 @@ namespace TravelRecordApp
                 var categories = (from p in postTable
                                   orderby p.CategoryId
                                   select p.CategoryName).Distinct().ToList();
+
+                var categories2 = postTable.OrderBy(p => p.CategoryId).Select(p => p.CategoryName).Distinct().ToList();
+
+                Dictionary<string, int> categoriesCount = new Dictionary<string, int>();
+
+                foreach(var category in categories)
+                {
+                    var count = (from post in postTable
+                                 where post.CategoryName == category
+                                 select post).ToList().Count;
+
+                    var count2 = postTable.Where(p => p.CategoryName == category).ToList().Count;
+
+                    categoriesCount.Add(category, count2);
+                }
+
+                categoriesListView.ItemsSource = categoriesCount;
 
                 postCountLabel.Text = postTable.Count.ToString();
             }
