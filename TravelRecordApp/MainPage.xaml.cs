32fbe7e20739c8dfe4ed4760c51a411907e84bfe
Stephen Clark <stephen.clark2@city-holdings.co.uk>
2020-10-03 00:56:51 +0100

    90

--- TravelRecordApp/MainPage.xaml.cs
+++ TravelRecordApp/MainPage.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using System.ComponentModel;
+using System.Linq;
+using TravelRecordApp.Model;
 using Xamarin.Forms;
 
 namespace TravelRecordApp
@@ -17,19 +19,32 @@ namespace TravelRecordApp
             iconImage.Source = ImageSource.FromResource("TravelRecordApp.Assets.Images.plane.png", assembly);
         }
 
-        void LoginButton_Clicked(System.Object sender, System.EventArgs e)
+        private async void LoginButton_Clicked(System.Object sender, System.EventArgs e)
         {
+
             bool isEmailEmpty = string.IsNullOrEmpty(emailEntry.Text);
             bool isPasswordEmpty = string.IsNullOrEmpty(passwordEntry.Text);
 
-            if (!isEmailEmpty && !isPasswordEmpty)
+            if (isEmailEmpty || isPasswordEmpty)
             {
-                Navigation.PushAsync(new HomePage());
+                
 
             }
             else
             {
-                // Somit missing
+                var user = (await App.MobileService.GetTable<Users>().Where(u => u.Email == emailEntry.Text).ToListAsync()).FirstOrDefault();
+
+                if (user != null)
+                {
+                    if (user.Password == passwordEntry.Text)
+                        await Navigation.PushAsync(new HomePage());
+                    else
+                        await DisplayAlert("Error", "Email or password are incorrect", "Ok");
+                }
+                else
+                {
+                    await DisplayAlert("Error", "There was an error loggin you in", "Ok");
+                }
             }
         }
 
